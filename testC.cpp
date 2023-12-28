9021f56fcec833817765c59c1708c4e42060e631
shun <samesamejinbei@gmail.com>
2023-12-28 10:37:09 +0900

    追加

--- testC.cpp
+++ testC.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <stdio.h>
+#include <vector>
+#include <string>
+
 
 int main()
 {
-  std::cout << "Hello World!" << std::endl;
-  printf("Heoo");
+  // std::cout << "Hello World!" << std::endl;
+  // printf("Heoo");
+
+ int n,a,b,c;
+     
+
+    std::cin >> n >> a >> b >> c;
+    
+    n = n + c;
+    a = a + b;
+    
+    std::cout << n << a << std::endl;
+    
+    //  std::cin.clear();
+    return 0;
+
+
   return 0;
 }
